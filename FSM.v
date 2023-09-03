cec86328648f85c3f0a0464cf723b76eccc59bd9
2Martina <Martina_magued@hotmail.com>
2023-09-03 19:54:30 +0300

    add
    add

---
+++ FSM.v
@@ -0,0 +1,109 @@
+module FSM
+#(parameter s0="IDLE",s1="READ_DATA",s2="READ_ADD",s3="CHK_CMD",s4="WRITE",Read_PARAM="Add")
+(
+    input clk,rst,
+    MOSI,SS_n,tx_valid,
+    input [7:0] tx_data,
+
+    output reg MISO,rx_valid,
+    output reg [9:0] rx_data
+);
+    
+    reg state,nextstate,Read;
+    reg [7:0] StoP_out;
+
+
+
+
+//State memory always block
+    always@(posedge clk or posedge rst)
+    begin
+    if (rst)
+    state<= s0;
+    else begin
+    state<= nextstate;
+    end
+    end
+
+
+
+//Next state logic always block
+    always@(*)
+    begin
+        case(state)
+            "IDLE": if(SS_n)
+                    nextstate = "IDLE" ;
+                    else
+                    nextstate = "CHK_CMD";
+
+
+            "CHK_CMD" : if(SS_n)
+                        nextstate = "IDLE";
+                        else if (SS_n==0 && MOSI==0)
+                        nextstate = "  WRITE";
+                        else if (SS_n==0 && MOSI==1 && Read=="Add")   
+                        nextstate = "READ_ADD";
+                        else 
+                        nextstate = "READ_DATA";
+                        
+
+            "WRITE" : if(SS_n)
+                      nextstate = "IDLE";
+                      else 
+                      nextstate = "WRITE";
+            
+
+          //  "READ_DATA" : 
+
+
+
+
+            //********when back to IDLE let Read=Data and later change it to Add????????/
+            "READ_ADD" : if(SS_n) 
+                         nextstate = "IDLE";
+                         else
+                         nextstate = "READ_ADD";
+        
+
+        endcase
+    end
+
+
+    StoP serial_to_par (clk,MOSI,StoP_out,finish);
+
+    //output logic always block
+    always@(posedge clk)
+     begin
+        case(state)
+            //"IDLE": 
+
+
+           //"CHK_CMD" : 
+           
+                        
+
+            "WRITE" : begin
+            rx_data = StoP_out;
+            rx_valid = finish;   
+            SS_n = 1;  //should be changed from the Master???
+            end
+
+            
+
+            "READ_DATA" : begin
+            Read ="DATA";   /////?????????????
+            end
+
+
+            "READ_ADD" : begin
+            rx_data = StoP_out;
+            rx_valid = finish;   
+            SS_n = 1;  //should be changed from the Master??
+            Read ="DATA";   /////?????????????
+            end
+
+        endcase
+     end
+
+
+endmodule
---
+++ StoP.v
@@ -0,0 +1,42 @@
+module StoP(
+input clk,in,
+output [7:0] out,
+output reg finish
+);
+
+reg [7:0] temp;
+reg [3:0] i; 
+
+initial begin
+    temp=0;
+    i=0;
+    //out = 0;
+    finish=0;
+end
+
+always@(negedge clk)//(posedge clk)
+begin
+   if(i<4'b1000)
+    temp <= {in,temp[7:1]}; //out >> 1;   //
+    i<=i+1;
+end
+assign out=temp;
+
+/*always@(negedge clk)
+begin
+    if(enable && i<4'b1001)
+       out <= {in,temp[6:0]};
+    else if (i==4'b1001)
+        finish <= 1;
+end*/
+/* add wave -position insertpoint sim:/StoP/*
+force -freeze sim:/StoP/clk 1 0, 0 {50 ps} -r 100
+force -freeze sim:/StoP/in 0 0
+run
+force -freeze sim:/StoP/in 1 0
+run*/
+
+
+
+
+endmodule
